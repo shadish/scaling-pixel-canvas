f412c7bee8a82e30bcfc9a32824489dcfe9c9215
shadish <shadtisch@gmail.com>
2018-07-14 14:33:18 -0600

    improve pixel rendering

--- src/runCanvas.js
+++ src/runCanvas.js
@@ -11,7 +11,6 @@ function getCanvas() {
 
 function lerp(a,b,t) {
 	let result = (1-t) * a + t * b
-	console.log("for:", a, b, t, "gets", Math.round(result))
 	return Math.round(result)
 }
 
@@ -19,21 +18,17 @@ function drawPixels() {
 	let c = getCanvas()
 	var ctx = c.getContext("2d");
 
-	let pixel = 20;
-	let size = { width: c.width, height: c.height}
-	let res = { width: 30, height: 30 }
+	let pixel = 16;
+	let max = c.width
 
-	let maxX = res.width * pixel
-	let maxY = res.height * pixel
-
-	for(let x = 0; x < maxX; x += pixel) {
-		for(let y = 0; y < maxY; y += pixel) {
-			let red = lerp(0,99,x/maxX)
-			red = red < 10 ? "0"+red : red
-			let green = lerp(0,99, y/maxY)
-			green = green < 10 ? "0"+green : green
-			ctx.fillStyle = `#${red}${green}00`
-			console.log("ctx:",ctx.fillStyle)
+	for(let x = 0; x <= max; x += pixel) {
+		for(let y = 0; y <= max; y += pixel) {
+			let xColor = lerp(0,255,x/max)
+			let yColor = lerp(0,255,y/max)
+			ctx.fillStyle = `rgba(${xColor},0,${yColor},1)`
+			if(y > 250) { 
+				console.log(x,y,'::',pixel)
+			}
 			ctx.fillRect(x,y,pixel,pixel)
 		}
 	}
@@ -42,10 +37,12 @@ function drawPixels() {
 function resize() {
 	let app = document.getElementById('app')
 	let canvas = getCanvas()
-	app.style.height = `${window.innerHeight-2}px`
-	canvas.width = `${window.innerHeight-4}`
-	canvas.height = `${window.innerHeight-4}`
-	console.log('resizing to:', canvas.style.height, canvas.style.width)
+	//temp: do not resize
+	// app.style.height = `${window.innerHeight-2}px`
+	// canvas.width = `${window.innerHeight-4}`
+	// canvas.height = `${window.innerHeight-4}`
+	canvas.width = 512
+	canvas.height = canvas.width
 	drawPixels()
 }
 
