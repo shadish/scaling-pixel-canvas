d1660ebb6ac7ecce23552127f99ecffa474cfacc
shadish <shadtisch@gmail.com>
2018-07-31 22:44:44 -0600

    fix animator

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { setup, tick } from './runCanvas';
+import {setup, tick} from './runCanvas';
+import './css/index.css';
 
-function runReact() {
-    ReactDOM.render(<App />, document.getElementById('app'));
-    registerServiceWorker();
+const title = 'Template Title';
 
-    // Initialize and draw immediately.
-    setup();
-    tick();
+ReactDOM.render(
+  <div>
+    {title}
+    <App />
+  </div>,
+  document.getElementById('app')
+);
 
-    // Begin animation loop. Note: still need to determine the most effecient draw cycle.
-    // The 'requestAnimationFrame' call may render better, but initial exploration determined
-    // it is too expensive (since what we do in tick is expensive).
-    window.setInterval(tick, 1000 / 10);
-}
+setup();
+tick();
+window.setInterval(tick, 1000/10);
+
+module.hot.accept();
